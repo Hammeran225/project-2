723a359975fa19c83a8f67853ef2ab0cbb106c3a
Hammeran225 <57191815+Hammeran225@users.noreply.github.com>
2023-03-30 12:26:57 -0500

    test 1

---
+++ js/scripts.js
@@ -0,0 +1,96 @@
+// scripts.js
+
+const quoteContainer = document.getElementById('quote-container');
+
+function createShatterEffect() {
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  canvas.style.position = 'fixed';
+  canvas.style.top = '0';
+  canvas.style.left = '0';
+  canvas.style.pointerEvents = 'none';
+  document.body.appendChild(canvas);
+
+  const Engine = Matter.Engine;
+  const Render = Matter.Render;
+  const Runner = Matter.Runner;
+  const Composite = Matter.Composite;
+  const Composites = Matter.Composites;
+  const Common = Matter.Common;
+  const MouseConstraint = Matter.MouseConstraint;
+  const Mouse = Matter.Mouse;
+  const World = Matter.World;
+  const Bodies = Matter.Bodies;
+
+  const engine = Engine.create();
+  const world = engine.world;
+
+  const render = Render.create({
+    canvas: canvas,
+    engine: engine,
+    options: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      wireframes: false,
+      background: 'transparent',
+    },
+  });
+
+  Render.run(render);
+
+  const runner = Runner.create();
+  Runner.run(runner, engine);
+
+  const group = Body.nextGroup(true);
+
+  const stack = Composites.stack(
+    quoteContainer.offsetLeft,
+    quoteContainer.offsetTop,
+    5,
+    5,
+    0,
+    0,
+    (x, y) => {
+      const options = {
+        render: {
+          fillStyle: '#fff',
+          strokeStyle: '#000',
+          lineWidth: 1,
+        },
+        collisionFilter: {
+          group: group,
+        },
+      };
+      return Bodies.rectangle(x, y, 20, 20, options);
+    },
+  );
+
+  World.add(world, stack);
+
+  const mouse = Mouse.create(render.canvas);
+  const mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+      render: {
+        visible: false,
+      },
+    },
+  });
+
+  World.add(world, mouseConstraint);
+  render.mouse = mouse;
+
+  setTimeout(() => {
+    document.body.removeChild(canvas);
+    Matter.Composite.clear(engine.world);
+    Matter.Engine.clear(engine);
+    Matter.Render.stop(render);
+    Matter.Runner.stop(runner);
+  }, 5000);
+}
+
+quoteContainer.addEventListener('click', () => {
+  createShatterEffect();
+});
