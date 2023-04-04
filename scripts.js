c3f630786eb81b2da7f8cb68cdde2f08e9f8f11e
Hammeran225 <57191815+Hammeran225@users.noreply.github.com>
2023-04-03 23:03:30 -0500

    test

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
-const quoteContainer = document.getElementById('quote-container');
-const whiteBox = document.getElementById('white-box');
+function createRain() {
+  const rain = document.createElement('div');
+  rain.classList.add('rain');
+  document.body.appendChild(rain);
 
-quoteContainer.addEventListener('click', () => {
-  whiteBox.style.opacity = '1';
-  whiteBox.innerHTML = 'nothing '.repeat(10);
-});
+  for (let i = 0; i < 150; i++) {
+    const drop = document.createElement('div');
+    drop.classList.add('drop');
+    drop.style.left = `${Math.random() * 100}vw`;
+    drop.style.animationDuration = `${Math.random() * 1 + 0.5}s`;
+    drop.style.animationDelay = `${Math.random() * 2}s`;
+    rain.appendChild(drop);
+  }
+}
+
+createRain();
